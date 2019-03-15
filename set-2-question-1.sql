947f9082dca4dad5f45bb47de387ffc99ca14c72
decarvalhohenrique <47057769+decarvalhohenrique@users.noreply.github.com>
2019-03-15 04:10:31 -0300

    Update set-2-question-1.sql

--- set-2-question-1.sql
+++ set-2-question-1.sql
@@ -1,22 +1,25 @@
+/* Udacity 1st project: Investigate a Relational Database */
 
+/* Question Set # 2 */
 
 /*
+Question 1:
 We want to find out how the two stores compare in their count of rental orders during every month for all the years 
 we have data for. Write a query that returns the store ID for the store, the year and month and the number of rental
 orders each store has fulfilled for that month. Your table should include a column for each of the following: year, month, 
 store ID and count of rental orders fulfilled during that month.
 */
 
-SELECT 	DATE_PART('month', rental_date) AS rental_month, 
-		DATE_PART('year', rental_date) AS rental_year,
-		se.store_id,
-		COUNT(*)
-FROM store se
-JOIN staff sf
-ON se.store_id = sf.store_id
-JOIN payment pt
-ON sf.staff_id = pt.staff_id
-JOIN rental rl
-ON rl.rental_id = pt.rental_id
-GROUP BY 1, 2, 3
-ORDER BY 2, 1;
+SELECT DATE_PART('month', r1.rental_date) AS rental_month, 
+       DATE_PART('year', r1.rental_date) AS rental_year,
+       s1.store_id,
+       COUNT(*)
+  FROM store AS s1
+       JOIN staff AS s2
+        ON s1.store_id = s2.store_id
+       JOIN payment p
+        ON s2.staff_id = p.staff_id
+       JOIN rental r1
+        ON r1.rental_id = p.rental_id
+ GROUP BY 1, 2, 3
+ ORDER BY 2, 1;
