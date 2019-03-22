cb21d0b9f628b4afb58c29dd6ed90e9272ce5f75
decarvalhohenrique <47057769+decarvalhohenrique@users.noreply.github.com>
2019-03-22 08:12:36 -0300

    Update third-presentation-query.sql

--- third-presentation-query.sql
+++ third-presentation-query.sql
@@ -1,32 +1,33 @@
+/* ---------- Query 3 - Amount spent by the top 10 paying customers between February and April 2007 ---------- */
 
 WITH t1 AS (SELECT (first_name || ' ' || last_name) AS name, 
                    c.customer_id, 
                    p.amount, 
-                   DATE_TRUNC('month', p.payment_date) AS payment_date,
-			       p.rental_id
+                   p.payment_date
               FROM customer AS c
                    JOIN payment AS p
                     ON c.customer_id = p.customer_id),
-t2 AS (SELECT 
-t1.name,
-              t1.payment_date,
-              SUM(t1.amount),
-              LEAD(SUM(t1.amount)) OVER(PARTITION BY t1.name ORDER BY DATE_TRUNC('month', t1.payment_date)) AS lead,
-              LEAD(SUM(t1.amount)) OVER(PARTITION BY t1.name ORDER BY DATE_TRUNC('month', t1.payment_date)) - SUM(t1.amount) AS lead_dif,
-			        CASE 
-           WHEN LEAD(SUM(t1.amount)) OVER(PARTITION BY t1.name ORDER BY DATE_TRUNC('month', t1.payment_date)) - SUM(t1.amount) < 0 THEN 0
-           WHEN LEAD(SUM(t1.amount)) OVER(PARTITION BY t1.name ORDER BY DATE_TRUNC('month', t1.payment_date)) - SUM(t1.amount) >= 0 THEN 1
-           END AS progress
-FROM t1
-JOIN rental AS r
-ON r.rental_id = t1.rental_id
-AND t1.customer_id = r.customer_id
-GROUP BY 1, 2)
-										  
-SELECT t2.payment_date,
-       COUNT(*) AS total_count,
-       SUM(t2.progress) AS progress_bymon,
-       COUNT(*) - SUM(t2.progress) AS regress_bymon
-FROM t2
-GROUP BY 1
-ORDER BY 1;
+
+     t2 AS (SELECT t1.customer_id
+              FROM t1
+             GROUP BY 1
+             ORDER BY SUM(t1.amount) DESC
+             LIMIT 10),
+
+     t3 AS (SELECT t1.name,
+                   DATE_TRUNC('month', t1.payment_date) AS pay_mon, 
+                   COUNT(*) AS pay_countpermon,
+                   SUM(t1.amount) AS pay_amount
+              FROM t1
+                   JOIN t2
+                    ON t1.customer_id = t2.customer_id
+              WHERE t1.payment_date BETWEEN '20070101' AND '20080101'
+              GROUP BY 1, 2
+              ORDER BY 1, 3, 2)
+
+SELECT t3.name,
+       t3.pay_mon,
+       t3.pay_amount,
+	    ROUND(AVG(t3.pay_amount) OVER (PARTITION BY t3.name), 2) AS avg_amount					
+  FROM t3
+ ORDER BY 1, 2;
