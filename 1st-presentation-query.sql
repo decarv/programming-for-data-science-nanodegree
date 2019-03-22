7abe7fa1c94f0708f54c44e7f91cfb33f5c52a11
decarvalhohenrique <47057769+decarvalhohenrique@users.noreply.github.com>
2019-03-22 08:13:59 -0300

    Update 1st-presentation-query.sql

--- 1st-presentation-query.sql
+++ 1st-presentation-query.sql
@@ -1,13 +1,17 @@
-WITH t1 AS (SELECT DISTINCT c3.customer_id, p.rental_id
+
+/* ---------- Query 1 - Number of rentals of top 10 renting countries ---------- */
+
+WITH t1 AS (SELECT c3.customer_id, 
+                   p.rental_id
               FROM country AS c1
                    JOIN city AS c2
-                   ON c1.country_id = c2.country_id
+                    ON c1.country_id = c2.country_id
                    JOIN address a
-                  ON c2.city_id = a.city_id
+                    ON c2.city_id = a.city_id
                    JOIN customer c3
-                  ON a.address_id = c3.address_id
+                    ON a.address_id = c3.address_id
                    JOIN payment p
-                  ON c3.customer_id = p.customer_id
+                    ON c3.customer_id = p.customer_id
                    JOIN (
                         SELECT c1.country_id
                           FROM country AS c1
@@ -24,33 +28,33 @@ WITH t1 AS (SELECT DISTINCT c3.customer_id, p.rental_id
                          LIMIT 10) sub
                  ON sub.country_id = c1.country_id),
 					
-t2 AS (SELECT c.name,
-              COUNT(r.rental_id) AS count_top10
-         FROM t1
-              JOIN rental AS r
-               ON r.rental_id = t1.rental_id
-              JOIN inventory AS i
-               ON i.inventory_id = r.inventory_id
-              JOIN film f
-               ON f.film_id = i.film_id
-              JOIN film_category fc
-               ON f.film_id = fc.film_id
-              JOIN category c
-               ON c.category_id = fc.category_id
-        GROUP BY 1),
+     t2 AS (SELECT c.name,
+                   COUNT(r.rental_id) AS count_top10
+              FROM t1
+                   JOIN rental AS r
+                    ON r.rental_id = t1.rental_id
+                   JOIN inventory AS i
+                    ON i.inventory_id = r.inventory_id
+                   JOIN film f
+                    ON f.film_id = i.film_id
+                   JOIN film_category fc
+                    ON f.film_id = fc.film_id
+                   JOIN category c
+                    ON c.category_id = fc.category_id
+             GROUP BY 1),
 
-t3 AS (SELECT c.name,
-              COUNT(r.rental_id) AS rental_count
-         FROM rental AS r
-              JOIN inventory AS i
-               ON i.inventory_id = r.inventory_id
-              JOIN film f
-               ON f.film_id = i.film_id
-              JOIN film_category fc
-               ON f.film_id = fc.film_id
-              JOIN category c
-               ON c.category_id = fc.category_id
-        GROUP BY 1)
+     t3 AS (SELECT c.name,
+                   COUNT(r.rental_id) AS rental_count
+              FROM rental AS r
+                   JOIN inventory AS i
+                    ON i.inventory_id = r.inventory_id
+                   JOIN film f
+                    ON f.film_id = i.film_id
+                   JOIN film_category fc
+                    ON f.film_id = fc.film_id
+                   JOIN category c
+                    ON c.category_id = fc.category_id
+             GROUP BY 1)
 		
 SELECT t2.name AS category,
        t3.rental_count - t2.count_top10 AS other_countries,
